73d6ef7d8e05de59bc6a0e2316f11acde977e925
911jenil <jenil9112005@gmail.com>
2025-02-10 16:33:21 +0530

    today

---
+++ Demo9/forgetpassword.swift
@@ -0,0 +1,18 @@
+//
+//  forgetpassword.swift
+//  Demo9
+//
+//  Created by student on 10/02/25.
+//
+
+import SwiftUI
+
+struct forgetpassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    forgetpassword()
+}
